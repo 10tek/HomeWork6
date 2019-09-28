d70cde347747edd62288c2f1aacf7161c05c2b6f
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-28 19:58:08 +0600

    MyArray класс-массив. При установке значения будет возводить в квадрат.

---
+++ HW6/MyArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HW6
+{
+    public class MyArray
+    {
+        private int[] _array;
+        public int Length { get; set; }
+
+        public MyArray()
+        {
+            Length = 0;
+            _array = new int[Length];
+        }
+
+        public MyArray(int Length)
+        {
+            Length = Length;
+            _array = new int[Length];
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return _array[i];
+            }
+            set
+            {
+                _array[i] = (int)Math.Pow(value, 2);
+            }
+        }
+
+        public void Add(int number)
+        {
+            int[] newArray = new int[++Length];
+            for(int i = 0; i < newArray.Length; i++)
+            {
+                if (i == Length - 1) //последняя итерация
+                {
+                    newArray[i] = number;
+                    break;
+                }
+                newArray[i] = _array[i];
+            }
+            _array = newArray;
+        }
+    }
+}
---
+++ HW6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray array = new MyArray();
+            array.Add(5);
+            array.Add(52);
+            array.Add(51);
+            array[0] = 7;
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
